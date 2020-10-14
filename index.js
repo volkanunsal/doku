6defe0518be9f7211eec1aba2a3da8039c255898
Volkan Unsal <vu@amazon.com>
2020-10-14 13:21:11 -0400

    Add static file server

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 const fg = require('fast-glob');
+const express = require('express');
+const app = express();
+
 const argv0 = require('minimist');
 
 const main = async () => {
@@ -17,6 +20,8 @@ Options:
 `);
     return;
   }
+  const fileServerUrl = 'http://localhost:9999/';
+
   const browser = await puppeteer.launch({ headless: false });
   fileName = fileName.endsWith('.pdf')
     ? fileName.replace('.pdf', '')
@@ -35,77 +40,94 @@ Options:
 
   // FIXME: table of contents not showing up.
 
-  try {
-    await page.setContent(
-      `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title></title>
-          <style type='text/css'>
-            @import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
-            @import 'https://use.fontawesome.com/releases/v5.14.0/css/all.css';
-          </style>
-          <meta charSet='utf-8' />
-          <meta httpEquiv='x-ua-compatible' content='ie=edge' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <script
-            src='https://code.jquery.com/jquery-3.5.1.min.js'
-            type='text/javascript'
-            async
-          />
-          <script
-            src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js'
-            type='text/javascript'
-            async
-          />
-          <script src="https://gist.githubusercontent.com/volkanunsal/4f6a0e1d52f871b970feb7c0326872ab/raw/331a7625f6f234f413f05729a7a10d5b3307d020/toc.js" async></script>
-          <script type='text/javascript'>
-            (() => {
-              tableOfContents('#toc', '#toc', { levels: 'h1, h2, h3, h4' });
-            })();
-          </script>
+  app.use(
+    '/',
+    express.static(process.cwd() + '/', {
+      setHeaders: (res) => {
+        res.header('Access-Control-Allow-Origin', '*');
+      },
+    })
+  );
 
-          <script type="module" src="https://cdn.jsdelivr.net/npm/zero-md@next/dist/zero-md.min.js" async></script>
-        </head>
-        <body>
-          <div className='top'>
-            <main className='container'>
-              <div className='row'>
-                <article className='col-lg-12'>
-  ${entries.map(
-    (v) => `<zero-md no-shadow src="file://${process.cwd()}/${v}"></zero-md>`
-  )}
-                </article>
-              </div>
-            </main>
+  const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title></title>
+      <style type='text/css'>
+        @import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
+        @import 'https://use.fontawesome.com/releases/v5.14.0/css/all.css';
+      </style>
+      <meta charSet='utf-8' />
+      <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <script
+        src='https://code.jquery.com/jquery-3.5.1.min.js'
+        type='text/javascript'
+        async
+      />
+      <script
+        src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js'
+        type='text/javascript'
+        async
+      />
+      <script src="https://gist.githubusercontent.com/volkanunsal/4f6a0e1d52f871b970feb7c0326872ab/raw/331a7625f6f234f413f05729a7a10d5b3307d020/toc.js" async></script>
+      <script type='text/javascript'>
+        (() => {
+          tableOfContents('#toc', '#toc', { levels: 'h1, h2, h3, h4' });
+        })();
+      </script>
+
+      <script type="module" src="https://cdn.jsdelivr.net/npm/zero-md@next/dist/zero-md.min.js" async></script>
+    </head>
+    <body>
+      <div className='top'>
+        <main className='container'>
+          <div className='row'>
+            <article className='col-lg-12'>
+            <aside id='toc'>
+${entries
+  .map((v) => `<zero-md no-shadow src="${fileServerUrl}${v}"></zero-md>`)
+  .join('')}
+            </aside>
+            </article>
           </div>
-        </body>
-      </html>
-    `,
-      { waitUntil: 'networkidle2' }
-    );
+        </main>
+      </div>
+    </body>
+  </html>
+`;
+  app.get('/', (_, res) => {
+    res.set('Content-Type', 'text/html');
+    res.send(Buffer.from(html));
+  });
+  app.listen(9999);
 
-    // await page.pdf({
-    //   path: `./${fileName}.pdf`,
-    //   headerTemplate: '<div></div>',
-    //   footerTemplate:
-    //     "<div style='width: 100%; text-align: right; font-size: 10px; color: #333; padding-right: 30px;'><span class='pageNumber'></span></div>",
-    //   displayHeaderFooter: true,
+  try {
+    await page.goto(fileServerUrl, { waitUntil: 'networkidle2' });
 
-    //   format: 'A4',
-    //   margin: {
-    //     bottom: 70,
-    //     left: 10,
-    //     right: 10,
-    //     top: 70,
-    //   },
-    // });
+    await page.pdf({
+      path: `./${fileName}.pdf`,
+      headerTemplate: '<div></div>',
+      footerTemplate:
+        "<div style='width: 100%; text-align: right; font-size: 10px; color: #333; padding-right: 30px;'><span class='pageNumber'></span></div>",
+      displayHeaderFooter: true,
+
+      format: 'A4',
+      margin: {
+        bottom: 70,
+        left: 10,
+        right: 10,
+        top: 70,
+      },
+    });
   } catch (error) {
     console.log(error);
   }
+
   console.log('PDF file created:', fileName + '.pdf');
-  // await browser.close();
+  await browser.close();
+  process.exit(1);
 };
 
 module.exports.main = main;
